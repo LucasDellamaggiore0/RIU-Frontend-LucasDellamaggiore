de5877215153bd786fa7a500a5ac59758b7fba31
Lucas Dellamaggiore <dellamaggiorelucas25@gmail.com>
2025-04-14 17:40:08 -0300

    Configure routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HeroesComponent } from './components/heroes/heroes/heroes.component';
+import { HeroFormComponent } from './components/hero-form/hero-form/hero-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'heroes', pathMatch: 'full' },
+    { path: 'heroes', component: HeroesComponent },
+    { path: 'add', component: HeroFormComponent },
+    { path: 'edit/:id', component: HeroFormComponent }
+];
