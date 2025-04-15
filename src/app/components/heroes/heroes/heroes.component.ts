bc4180a6e8b91d1f1fb1f03f8dd8e2ff5687abce
Lucas Dellamaggiore <dellamaggiorelucas25@gmail.com>
2025-04-15 09:24:21 -0300

    The home page is created with all the superheroes, their search bar and the page layout

--- src/app/components/heroes/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes/heroes.component.ts
@@ -1,11 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HeroCardComponent } from '../../hero-card/hero-card/hero-card.component';
+import { HeroService } from '../../../services/hero.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Hero } from '../../../models/hero.model';
 
 @Component({
   selector: 'app-heroes',
-  imports: [],
+  imports: [HeroCardComponent, FormsModule, CommonModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
-export class HeroesComponent {
 
+
+export class HeroesComponent {
+  private heroService = inject(HeroService);
+  heroes: Hero[] = [];
+  filteredHeroes: Hero[] = [];
+  searchHeroQuery = '';
+  currentPage = 1;
+  itemsPerPage = 6;
+
+  constructor() {
+    this.heroService.getAllHeroes().subscribe(heroes => {
+      this.heroes = heroes;
+      this.filteredHeroes = [...heroes]
+    })
+  }
+
+  get paginatedHeroes(): Hero[] {
+    const initialPage = (this.currentPage - 1) * this.itemsPerPage;
+    return this.filteredHeroes.slice(initialPage, initialPage + this.itemsPerPage)
+  }
+
+  nextPage() : void{
+    if ((this.currentPage * this.itemsPerPage) < this.filteredHeroes.length) {
+      this.currentPage++;
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  onSearch() {
+    this.currentPage = 1;
+    const term = this.searchHeroQuery.toLowerCase();
+    this.filteredHeroes = this.heroes.filter(hero =>
+      hero.name.toLowerCase().includes(term)
+    );
+  }
+
+  editHero(id: number) {
+    console.log('Editar héroe:', id);
+    // Lógica para navegación o modal
+  }
+
+  deleteHero(id: number) {
+    if (confirm('¿Estás seguro de eliminar este héroe?')) {
+      this.heroService.removeHero(id);
+    }
+  }
+
+  addHero() {
+    console.log('Añadir nuevo héroe');
+    // Lógica para agregar
+  }
 }
