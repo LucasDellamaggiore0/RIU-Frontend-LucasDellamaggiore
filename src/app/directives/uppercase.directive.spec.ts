059fe55ae6179ef3b47440a5faaf614f85bed3b4
Lucas Dellamaggiore <dellamaggiorelucas25@gmail.com>
2025-04-15 12:18:34 -0300

    Unit tests are created for uppercase.directive

--- src/app/directives/uppercase.directive.spec.ts
+++ src/app/directives/uppercase.directive.spec.ts
@@ -1,8 +1,35 @@
 import { UppercaseDirective } from './uppercase.directive';
+import { ElementRef, Renderer2 } from '@angular/core';
 
 describe('UppercaseDirective', () => {
+  let directive: UppercaseDirective;
+  let el: ElementRef;
+  let renderer: Renderer2;
+
+  beforeEach(() => {
+    el = { nativeElement: document.createElement('div') } as ElementRef;
+    renderer = { setProperty: jasmine.createSpy('setProperty') } as unknown as Renderer2;
+    
+    directive = new UppercaseDirective(el, renderer);
+  });
+
   it('should create an instance', () => {
-    const directive = new UppercaseDirective();
     expect(directive).toBeTruthy();
+  });
+
+  it('should transform text to uppercase when value changes', () => {
+    directive.value = 'hello';
+    directive.ngOnChanges({
+      value: {
+        currentValue: 'hello',
+        firstChange: true,
+        previousValue: undefined,
+        isFirstChange: function (): boolean {
+          throw new Error('Function not implemented.');
+        }
+      },
+    });
+
+    expect(renderer.setProperty).toHaveBeenCalledWith(el.nativeElement, 'textContent', 'HELLO');
   });
 });
