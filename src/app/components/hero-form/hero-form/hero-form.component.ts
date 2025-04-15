16f8312ab5fd006a6750443882f066cad3a54fb9
Lucas Dellamaggiore <dellamaggiorelucas25@gmail.com>
2025-04-15 11:33:26 -0300

    Form for uploading new superheroes with some validations is created.

--- src/app/components/hero-form/hero-form/hero-form.component.ts
+++ src/app/components/hero-form/hero-form/hero-form.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HeroService } from '../../../services/hero.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero-form',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './hero-form.component.html',
   styleUrl: './hero-form.component.css'
 })
 export class HeroFormComponent {
+  
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  private heroService = inject(HeroService);
+  newHeroForm: FormGroup;
+  
+  constructor() {
+    this.newHeroForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      description: [''],
+      power: ['', Validators.required],
+      height: ['', [Validators.required, Validators.min(1)]],
+      weight: ['', [Validators.required, Validators.min(1)]],
+      gender: ['', Validators.required]
+    })
+  }
 
+  onSubmitNewHeroeForm() {
+    if (this.newHeroForm.valid) {
+      const newHero = this.newHeroForm.value;
+      this.heroService.createNewHero(newHero);
+      console.log('Nuevo héroe:', newHero);
+      this.router.navigate(['/heroes']);
+    } else {
+      this.newHeroForm.markAllAsTouched();
+    }
+  }
+
+  onCancelSubmitNewHeroe() {
+    this.router.navigate(['/heroes'])
+  }
 }
